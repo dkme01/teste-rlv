2358198b818ef9eacbea20f78b5f6819c12493f3
dkme01 <dkme01@gmail.com>
2024-11-22 21:58:16 -0300

    [update] added a rule to fix production build

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack: (config) => {
+    config.module.rules.push({
+      test: /\.mjs$/,
+      include: /node_modules/,
+      type: "javascript/auto",
+    });
     config.resolve.alias.canvas = false
     return config
   }
