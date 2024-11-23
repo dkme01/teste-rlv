17f498a26c44564a4e70e81cc6eaef733d9ca326
dkme01 <dkme01@gmail.com>
2024-11-22 21:48:42 -0300

    [update] updated some base ui components to function properly or just overall visual style

--- src/app/documents/page.tsx
+++ src/app/documents/page.tsx
@@ -1,7 +1,28 @@
 import * as React from "react";
 
-export default function Documents() {
+import {promises as fs} from "fs"
+import path from "path"
+import {z} from "zod"
+
+import {columns} from "../components/table/columns"
+import {DataTable} from "../components/table/data-table"
+import {documentSchema} from "../components/table/data/schema"
+import {Separator} from "@/components/ui/separator";
+
+async function getTasks() {
+	const data = await fs.readFile(
+		path.join(process.cwd(), "src/app/components/table/data/documents.json")
+	)
+	
+	const documents = JSON.parse(data.toString())
+	
+	return z.array(documentSchema).parse(documents)
+}
+
+export default async function Documents() {
+	const tasks = await getTasks()
+	
 	return (
-		<>Documents</>
+		<DataTable data={tasks} columns={columns}/>
 	)
 }
